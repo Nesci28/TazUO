f715b80179443dcf75e8e102a3dfd1566642ce07
Marc G. <marc@okidoo.ca>
2026-07-05 18:48:34 -0400

    feat: default contextual menu action

--- src/ClassicUO.Client/Game/UI/Gumps/PopupMenuGump.cs
+++ src/ClassicUO.Client/Game/UI/Gumps/PopupMenuGump.cs
@@ -1,7 +1,10 @@
-﻿// SPDX-License-Identifier: BSD-2-Clause
+// SPDX-License-Identifier: BSD-2-Clause
 
+using System;
 using ClassicUO.Configuration;
 using ClassicUO.Game.Data;
+using ClassicUO.Game.GameObjects;
+using ClassicUO.Game.Managers;
 using ClassicUO.Game.UI.Controls;
 using ClassicUO.Input;
 using ClassicUO.Utility;
@@ -10,9 +13,11 @@ namespace ClassicUO.Game.UI.Gumps
 {
     public class PopupMenuGump : Gump
     {
+        private const ushort ClientActionHue = 0x03B2;
+
         public static uint CloseNext = uint.MaxValue;
 
-        private ushort _selectedItem;
+        private Action _selectedAction;
         private readonly PopupMenuData _data;
 
         public PopupMenuData Data => _data;
@@ -38,22 +43,18 @@ namespace ClassicUO.Game.UI.Gumps
             };
 
             Add(pic);
+
             int offsetY = ScaleHelper.Scaled(10, scale);
-            bool arrowAdded = false;
             int width = 0, height = ScaleHelper.Scaled(20, scale);
+            bool arrowAdded = false;
 
-            for (int i = 0; i < data.Items.Length; i++)
+            int AddRow(string text, ushort hue, ushort replacedHue, Action action)
             {
-                ref PopupMenuItem item = ref data.Items[i];
+                int rowY = offsetY;
 
-                string text = Client.Game.UO.FileManager.Clilocs.GetString(item.Cliloc);
-
-                ushort hue = item.Hue;
-
-                if (item.ReplacedHue != 0)
+                if (replacedHue != 0)
                 {
-                    uint h = (HuesHelper.Color16To32(item.ReplacedHue) << 8) | 0xFF;
-
+                    uint h = (HuesHelper.Color16To32(replacedHue) << 8) | 0xFF;
                     Client.Game.UO.FileManager.Fonts.SetUseHTML(true, h);
                 }
 
@@ -66,44 +67,92 @@ namespace ClassicUO.Game.UI.Gumps
 
                 var box = new HitBox(ScaleHelper.Scaled(10, scale), offsetY, label.Width, label.Height)
                 {
-                    Tag = item.Index
+                    Tag = action
                 };
 
                 box.MouseEnter += (sender, e) =>
                 {
-                    _selectedItem = (ushort)(sender as HitBox).Tag;
+                    _selectedAction = (Action)(sender as HitBox).Tag;
+                };
+
+                box.MouseUp += (sender, e) =>
+                {
+                    _selectedAction = (Action)(sender as HitBox).Tag;
                 };
 
                 Add(box);
                 Add(label);
 
+                offsetY += label.Height;
+                height += label.Height;
+
+                if (width < label.Width)
+                {
+                    width = label.Width;
+                }
+
+                return rowY;
+            }
+
+            for (int i = 0; i < data.Items.Length; i++)
+            {
+                ref PopupMenuItem item = ref data.Items[i];
+                ushort index = item.Index;
+                string text = MobileDefaultContextActionManager.GetActionName(item.Cliloc);
+
+                int rowY = AddRow(
+                    text,
+                    item.Hue,
+                    item.ReplacedHue,
+                    () =>
+                    {
+                        if (CUOEnviroment.Debug)
+                        {
+                            GameActions.Print(World, $"Popup menu [{_data.Serial}] response: {index}");
+                        }
+
+                        GameActions.ResponsePopupMenu(_data.Serial, index);
+                    }
+                );
+
                 if ((item.Flags & 0x02) != 0 && !arrowAdded)
                 {
                     arrowAdded = true;
 
-                    // TODO: wat?
                     var arrow = new Button(0, 0x15E6, 0x15E2, 0x15E2)
                     {
                         X = ScaleHelper.Scaled(20, scale),
-                        Y = offsetY
+                        Y = rowY
                     };
+
                     arrow.ApplyScale(scale, scalePosition: false);
-
                     Add(arrow);
+                }
+            }
 
-                    height += ScaleHelper.Scaled(20, scale);
+            if (World.Get(data.Serial) is Mobile mobile)
+            {
+                for (int i = 0; i < data.Items.Length; i++)
+                {
+                    PopupMenuItem item = data.Items[i];
+                    string actionName = MobileDefaultContextActionManager.GetActionName(item.Cliloc);
+
+                    AddRow(
+                        $"Set default: {actionName}",
+                        ClientActionHue,
+                        0,
+                        () => MobileDefaultContextActionManager.SetDefault(mobile, item)
+                    );
                 }
 
-                offsetY += label.Height;
-
-                if (!arrowAdded)
+                if (MobileDefaultContextActionManager.TryGetDefault(mobile, out _))
                 {
-                    height += label.Height;
-
-                    if (width < label.Width)
-                    {
-                        width = label.Width;
-                    }
+                    AddRow(
+                        "Clear default action",
+                        ClientActionHue,
+                        0,
+                        () => MobileDefaultContextActionManager.ClearDefault(mobile)
+                    );
                 }
             }
 
@@ -129,10 +178,7 @@ namespace ClassicUO.Game.UI.Gumps
         {
             if (button == MouseButtonType.Left)
             {
-                if (CUOEnviroment.Debug)
-                    GameActions.Print(World, $"Popup menu [{_data.Serial}] response: {_selectedItem}");
-
-                GameActions.ResponsePopupMenu(_data.Serial, _selectedItem);
+                _selectedAction?.Invoke();
                 Dispose();
             }
         }
