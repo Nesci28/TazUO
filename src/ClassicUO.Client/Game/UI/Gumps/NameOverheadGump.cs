59bd899c740e7b31cddd42fb627010d905e45491
Marc G. <marc@okidoo.ca>
2026-07-06 07:56:27 -0400

    fix: dispose

--- src/ClassicUO.Client/Game/UI/Gumps/NameOverheadGump.cs
+++ src/ClassicUO.Client/Game/UI/Gumps/NameOverheadGump.cs
@@ -366,12 +366,6 @@ namespace ClassicUO.Game.UI.Gumps
             base.CloseWithRightClick();
         }
 
-        public override void Dispose()
-        {
-            ClearBuffTimerTexts();
-            base.Dispose();
-        }
-
         private void DoDrag()
         {
             Point delta = Mouse.Position - _lastLeftMousePositionDown;
@@ -1764,6 +1758,7 @@ namespace ClassicUO.Game.UI.Gumps
 
         public override void Dispose()
         {
+            ClearBuffTimerTexts();
             _text?.Dispose();
             base.Dispose();
         }
