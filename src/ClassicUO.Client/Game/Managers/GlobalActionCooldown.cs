a926df6fc1153955ef9537f8824852119139df50
Marc G. <marc@okidoo.ca>
2026-07-21 15:26:38 -0400

    fix: retry rejected multi-moves after network delay

--- src/ClassicUO.Client/Game/Managers/GlobalActionCooldown.cs
+++ src/ClassicUO.Client/Game/Managers/GlobalActionCooldown.cs
@@ -4,10 +4,15 @@ namespace ClassicUO.Game.Managers;
 
 public static class GlobalActionCooldown
 {
+    public const long NetworkSafetyMargin = 50;
+
     private static long _nextActionTime = 0;
     private static long _cooldownDuration => ProfileManager.CurrentProfile?.MoveMultiObjectDelay ?? 1000;
     public static long CooldownDuration => _cooldownDuration;
 
     public static bool IsOnCooldown => Time.Ticks < _nextActionTime;
-    public static void BeginCooldown() => _nextActionTime = Time.Ticks + _cooldownDuration;
+    public static void BeginCooldown() => BeginCooldown(_cooldownDuration);
+
+    public static void BeginCooldown(long duration) =>
+        _nextActionTime = Time.Ticks + (duration > 0 ? duration : 0);
 }
