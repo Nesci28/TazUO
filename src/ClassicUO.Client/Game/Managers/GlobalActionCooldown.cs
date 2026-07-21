c82e20915f25c695f1d885879f7c5bb77c469ee3
Marc G. <marc@okidoo.ca>
2026-07-21 15:26:31 -0400

    fix: add safety margin to queued action delay

--- src/ClassicUO.Client/Game/Managers/GlobalActionCooldown.cs
+++ src/ClassicUO.Client/Game/Managers/GlobalActionCooldown.cs
@@ -5,14 +5,19 @@ namespace ClassicUO.Game.Managers;
 
 public static class GlobalActionCooldown
 {
+    public const long NetworkSafetyMargin = 50;
+
     private static long _nextActionTime = 0;
     private static long BaseCooldownDuration => ProfileManager.CurrentProfile?.MoveMultiObjectDelay ?? 1000;
 
     public static long CooldownDuration => BaseCooldownDuration;
-    public static long QueuedCooldownDuration => BaseCooldownDuration + CurrentPing;
+    public static long QueuedCooldownDuration => BaseCooldownDuration + CurrentPing + NetworkSafetyMargin;
     public static uint CurrentPing => AsyncNetClient.Socket?.Statistics?.Ping ?? 0;
 
     public static bool IsOnCooldown => Time.Ticks < _nextActionTime;
     public static void BeginCooldown(bool includePing = false) =>
-        _nextActionTime = Time.Ticks + (includePing ? QueuedCooldownDuration : CooldownDuration);
+        BeginCooldown(includePing ? QueuedCooldownDuration : CooldownDuration);
+
+    public static void BeginCooldown(long duration) =>
+        _nextActionTime = Time.Ticks + (duration > 0 ? duration : 0);
 }
