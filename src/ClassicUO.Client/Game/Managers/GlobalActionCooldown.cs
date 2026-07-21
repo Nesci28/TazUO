bb09516e8d374d3d747bf04eb428bda4180f23cb
Marc G. <marc@okidoo.ca>
2026-07-21 15:38:26 -0400

    fix: scale retry safety margin with ping

--- src/ClassicUO.Client/Game/Managers/GlobalActionCooldown.cs
+++ src/ClassicUO.Client/Game/Managers/GlobalActionCooldown.cs
@@ -4,11 +4,17 @@ namespace ClassicUO.Game.Managers;
 
 public static class GlobalActionCooldown
 {
-    public const long NetworkSafetyMargin = 50;
+    public const uint MaxNetworkSafetyMargin = 50;
 
     private static long _nextActionTime = 0;
     private static long _cooldownDuration => ProfileManager.CurrentProfile?.MoveMultiObjectDelay ?? 1000;
     public static long CooldownDuration => _cooldownDuration;
+
+    public static uint GetNetworkSafetyMargin(uint ping)
+    {
+        uint halfPing = ping / 2;
+        return halfPing < MaxNetworkSafetyMargin ? halfPing : MaxNetworkSafetyMargin;
+    }
 
     public static bool IsOnCooldown => Time.Ticks < _nextActionTime;
     public static void BeginCooldown() => BeginCooldown(_cooldownDuration);
