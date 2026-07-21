805566b8e919efe2fcc76421c976cd5707ec1f43
Marc G. <marc@okidoo.ca>
2026-07-21 15:38:17 -0400

    fix: scale action safety margin with ping

--- src/ClassicUO.Client/Game/Managers/GlobalActionCooldown.cs
+++ src/ClassicUO.Client/Game/Managers/GlobalActionCooldown.cs
@@ -5,14 +5,28 @@ namespace ClassicUO.Game.Managers;
 
 public static class GlobalActionCooldown
 {
-    public const long NetworkSafetyMargin = 50;
+    public const uint MaxNetworkSafetyMargin = 50;
 
     private static long _nextActionTime = 0;
     private static long BaseCooldownDuration => ProfileManager.CurrentProfile?.MoveMultiObjectDelay ?? 1000;
 
     public static long CooldownDuration => BaseCooldownDuration;
-    public static long QueuedCooldownDuration => BaseCooldownDuration + CurrentPing + NetworkSafetyMargin;
+    public static long QueuedCooldownDuration
+    {
+        get
+        {
+            uint ping = CurrentPing;
+            return BaseCooldownDuration + ping + GetNetworkSafetyMargin(ping);
+        }
+    }
+
     public static uint CurrentPing => AsyncNetClient.Socket?.Statistics?.Ping ?? 0;
+
+    public static uint GetNetworkSafetyMargin(uint ping)
+    {
+        uint halfPing = ping / 2;
+        return halfPing < MaxNetworkSafetyMargin ? halfPing : MaxNetworkSafetyMargin;
+    }
 
     public static bool IsOnCooldown => Time.Ticks < _nextActionTime;
     public static void BeginCooldown(bool includePing = false) =>
