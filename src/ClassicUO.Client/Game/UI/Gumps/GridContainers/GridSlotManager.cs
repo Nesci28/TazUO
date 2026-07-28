dfff46da59db6ca3112d839bfd5413a4b93c9ed3
Marc G. <marc@okidoo.ca>
2026-07-28 07:49:08 -0400

    feat: add per-container item filters

--- src/ClassicUO.Client/Game/UI/Gumps/GridContainers/GridSlotManager.cs
+++ src/ClassicUO.Client/Game/UI/Gumps/GridContainers/GridSlotManager.cs
@@ -530,7 +530,7 @@ namespace ClassicUO.Game.UI.Gumps.GridContainers;
             return _containerContents;
         }
 
-        private bool SearchItemNameAndProps(string search, Item item)
+        internal bool SearchItemNameAndProps(string search, Item item)
         {
             if (item == null)
                 return false;
