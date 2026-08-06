e325f22087589895f090699e3fe1ead3a93668a6
Marc G. <marc@okidoo.ca>
2026-08-06 08:48:10 -0400

    fix: allow OSI corpse items through grid highlight

--- tests/ClassicUO.UnitTests/Game/Managers/AutoLootCorpseResolutionTest.cs
+++ tests/ClassicUO.UnitTests/Game/Managers/AutoLootCorpseResolutionTest.cs
@@ -2,6 +2,7 @@ using ClassicUO.Game;
 using ClassicUO.Game.Data;
 using ClassicUO.Game.GameObjects;
 using ClassicUO.Game.Managers;
+using ClassicUO.Game.UI.Gumps.GridHighLight;
 using Xunit;
 
 namespace ClassicUO.UnitTests.Game.Managers
@@ -29,7 +30,7 @@ namespace ClassicUO.UnitTests.Game.Managers
         }
 
         [Fact]
-        public void GetContainingCorpse_ResolvesOsiDeadMobileHierarchy()
+        public void GetContainingCorpse_ResolvesDirectOsiDeadMobileParent()
         {
             Client.UnitTestingActive = true;
             var world = new World();
@@ -38,13 +39,56 @@ namespace ClassicUO.UnitTests.Game.Managers
             {
                 const uint corpseObjectSerial = 0x80000001;
                 Item corpse = AddItem(world, 0x40000001, 0x2006);
-                Item container = AddItem(world, 0x40000002, container: corpseObjectSerial);
-                Item loot = AddItem(world, 0x40000003, container: container.Serial);
+                Item loot = AddItem(world, 0x40000002, container: corpseObjectSerial);
 
                 corpse.CorpseParent = corpseObjectSerial;
                 world.CorpseManager.Add(corpse.Serial, corpseObjectSerial, Direction.South, false);
 
+                Assert.True(loot.OnGround);
                 Assert.Same(corpse, AutoLootManager.GetContainingCorpse(world, loot));
+            }
+            finally
+            {
+                ClearWorld(world);
+            }
+        }
+
+        [Fact]
+        public void GridHighlightEligibility_AllowsDirectOsiDeadMobileParent()
+        {
+            Client.UnitTestingActive = true;
+            var world = new World();
+
+            try
+            {
+                const uint corpseObjectSerial = 0x80000001;
+                Item corpse = AddItem(world, 0x40000001, 0x2006);
+                Item loot = AddItem(world, 0x40000002, container: corpseObjectSerial);
+
+                corpse.CorpseParent = corpseObjectSerial;
+                world.CorpseManager.Add(corpse.Serial, corpseObjectSerial, Direction.South, false);
+
+                Assert.True(loot.OnGround);
+                Assert.True(GridHighlightData.IsEligibleItem(world, loot));
+            }
+            finally
+            {
+                ClearWorld(world);
+            }
+        }
+
+        [Fact]
+        public void GridHighlightEligibility_RejectsActualGroundItem()
+        {
+            Client.UnitTestingActive = true;
+            var world = new World();
+
+            try
+            {
+                Item item = AddItem(world, 0x40000001);
+
+                Assert.True(item.OnGround);
+                Assert.False(GridHighlightData.IsEligibleItem(world, item));
             }
             finally
             {
